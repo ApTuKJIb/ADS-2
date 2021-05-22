c48bac597ac3da756521b41fe98d4c43f8d936c4
ApTuKJIb <78952964+ApTuKJIb@users.noreply.github.com>
2021-05-22 14:21:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,10 +3,9 @@
 int countPairs1(int *arr, int len, int value) {
  int a = 0;
   for (int i = 0; i < len - 1; i++) {
-    for (int j = 0; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value)
   a++;
-      }
     }
   }
   return a;
@@ -18,24 +17,18 @@ int countPairs2(int *arr, int len, int value) {
   int right = len - 1;
   while (left < right - 1) {
   int mid = (left + right) / 2;
-    if (arr[mid] > value) {
-    right = mid;
-    } else {
-      left = mid + 1;
-  }
+    if (arr[mid] <= value)
+      left = mid;
+    else
+      right = mid;
 }
-  len = right;
-  for (int i = 0; i < len; i++) {
-    for (int j = 0; j > i; j--) {
-      if (arr[i] + arr[j] == value) {
+  len = right - 1;
+  for (int i = len; i >= 0; i--) {
+    for (int j = 0; j < i; j++) {
+      if (arr[i] + arr[j] == value)
         a++;
-      }
-      if (arr[i] + arr[j] < value) {
+      if (arr[i] + arr[j] > value)
         break;
-      }
-      if (arr[i] + arr[j] > value) {
-        continue;
-      }
     }
   }
   return a;
@@ -43,24 +36,31 @@ int countPairs2(int *arr, int len, int value) {
 
 int countPairs3(int *arr, int len, int value) {
   int a = 0;
-  for (int i = 0; i < len - 1; i++) {
-    int left = i + 1;
+    int left = 0;
     int right = len - 1;
-    int second = value - arr[i];
-    while (left < right) {
+    while (left < right - 1) {
       int mid = (left + right) / 2;
-      if arr[mid] < second) {
-        left = mid + 1; 
-      } else {
+      if arr[mid] <= value) {
+        left = mid;
+      else
         right = mid;
       }
+     len = right - 1;
+    for (int i = 0; i < len; i++) {
+        left = i + 1, right = len - 1;
+        int countNum = 0;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < (value - arr[i]))
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        while (arr[left] == (value - arr[i])) {
+            countNum++;
+            left++;
+        }
+        count += countNum;
     }
-    if (arr[left] == second) {
-      while (arr[left] == second) {
-        a++;
-        left++;
-      }
-    }
-  }
   return a;
 }
